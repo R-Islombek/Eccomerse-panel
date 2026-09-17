36d1d1200b8d2472fb32e290dd0fffaeac42ce8a
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-17 18:24:42 +0500

    salom

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,6 +4,7 @@ export const Main = () => {
   return (
     <>
     <h1>Dilshodbek</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo doloremque minus harum amet magni dicta nulla omnis distinctio odio deserunt totam quo voluptatum laudantium placeat, commodi nostrum! Fugiat, minima culpa.</p>
     </>
   )
 }
