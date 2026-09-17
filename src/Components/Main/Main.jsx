e81443bc8438186b87345a3ae4e4f18474e75981
R-Islombek <islombekraximjanov97dev@gmail.com>
2026-09-17 18:26:37 +0500

    card bolimi

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,6 +5,7 @@ export const Main = () => {
     <>
     <h1>Dilshodbek</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo doloremque minus harum amet magni dicta nulla omnis distinctio odio deserunt totam quo voluptatum laudantium placeat, commodi nostrum! Fugiat, minima culpa.</p>
+    
     </>
   )
 }
