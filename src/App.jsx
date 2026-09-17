5e66f85998766c17e242cd099b8ecc7219a5fac7
Adhambek <ochilovadham188@gmail.com>
2026-09-17 17:49:32 +0500

    Adxambek

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ function App() {
     <>
        <h1>Hello world</h1>
        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, architecto?</p>
+       <del>Ochilov</del>
     </>
   )
 }
