3a253e1c02a0945dbc7036a9489eaee210703ff7
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-17 18:00:18 +0500

    push

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Main = () => {
+  return (
+    <>
+    <h1>Dilshodbek</h1>
+    <img src="http://localhost:5000/uploads/photo.jpg" alt="" />
+    </>
+  )
+}
