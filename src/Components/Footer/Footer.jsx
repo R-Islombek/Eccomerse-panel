d6039c6b12042d8997bc166a4e021861cd26b637
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-22 16:47:37 +0500

    link 2 qshildi

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -37,7 +37,21 @@ const Footer = () => {
                     <a className='footer__link' href="#">Мультимедиа</a>
                     <a className='footer__link' href="#">Soft skills</a>
                   </li>
-                  <li className='footer__item'></li>
+                  <li className='footer__item'>
+                    <a className='footer__link' href="#">Подросткам</a>
+                    <a className='footer__link' href="#">Детям</a>
+                    <a className='footer__link' href="#">Робототехника</a>
+                    <a className='footer__link' href="#">Создание игр</a>
+                    <a className='footer__link' href="#">Программирование</a>
+                    <a className='footer__link' href="#">Мультимедиа</a>
+                    <a className='footer__link' href="#">Soft skills</a>
+                    <a className='footer__link' href="#">Шахматы</a>
+                    <a className='footer__link' href="#">Блогинг</a>
+                    <a className='footer__link' href="#">Создание игр</a>
+                    <a className='footer__link' href="#">Программирование</a>
+                    <a className='footer__link' href="#">Мультимедиа</a>
+                    <a className='footer__link' href="#">Soft skills</a>
+                  </li>
                   <li className='footer__item'></li>
                   <li className='footer__item'></li>
                 </ul>
